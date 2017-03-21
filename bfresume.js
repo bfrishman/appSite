09e7ca747f1a245be03508a28ef49afe308ca374
Benjamin Frishman <bfrishman@gmail.com>
2017-03-21 02:50:01 -0500

    condensed some tabs, add backgrounds, fixed some positioning

--- bfresume.js
+++ bfresume.js
@@ -2,41 +2,46 @@ skills = [80, 70, 50, 50, 90];
 skillNum = 5;
 
 window.onload = function() {
+	$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
+	$("#place").load("default.html");	
+	$(document).ready(function(){
+		$("#skills").click(function(){
 			$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
-			$("#place").load("default.html");	
-			$(document).ready(function(){
-			
-				$("#skills").click(function(){
-					$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
-					$("#place").load("skills.html");
-				});
-				$("#education").click(function(){
-					$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
-					$("#place").load("education.html");
-				});
-				$("#experience").click(function(){
-					$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
-					$("#place").load("experience.html");
-				});
-				$("#about").click(function(){
-					$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
-					$("#place").load("about.html");
-				});		
-				$("#school").click(function(){
-					$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
-					$("#place").load("school.html");
-				});					
-			});	
-	function setNamePlate(x){
+			$("#place").load("skills.html");
+		});
+		$("#education").click(function(){
+			$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
+			$("#place").load("education.html");
+		});
+		$("#experience").click(function(){
+			$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
+			$("#place").load("experience.html");
+		});
+		$("#about").click(function(){
+			$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
+			$("#place").load("about.html");
+		});		
+		$("#school").click(function(){
+			$('head').append('<link rel="stylesheet" href="bfresume.css" type="text/css" />');
+			$("#place").load("school.html");
+		});					
+	});	
+	function setMainTopMargin(x){
 		x += "px";
 		document.getElementById("mainBox").style.marginTop = x;
 	}
-	function moveNPtoNavB() {
+	function moveMaintoNavB() {
 		var tall = document.getElementById("myNavBar").clientHeight;
-		setNamePlate(tall);
+		setMainTopMargin(tall);
 	}
-	moveNPtoNavB();
-	function sideBarHeight(SBheight){
+	moveMaintoNavB();
+	/*function changeHeight() { //fixing the height of the page...
+		var windowHeight = $(window).height();
+		var tall = document.getElementById("myNavBar").clientHeight;
+		console.log(windowHeight - tall);
+		document.getElementById("mainBox").style.height = ((windowHeight - tall) + "px");
+	}*/
+	/*function sideBarHeight(SBheight){
 		SBheight += "px";
 		document.getElementById("sidebar").style.height = SBheight;
 	}
@@ -44,5 +49,5 @@ window.onload = function() {
 		var Mheight = document.getElementById("mainBox").clientHeight;
 		sideBarHeight(Mheight);
 	}
-	resizeSideBar();	
+	resizeSideBar();	*/
 };
